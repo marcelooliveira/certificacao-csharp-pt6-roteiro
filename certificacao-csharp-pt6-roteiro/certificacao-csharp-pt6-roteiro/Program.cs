552014e91c2f2bbd546d49ba36c83a6b648801f5
marcelooliveira <mclricardo@gmail.com>
2018-09-13 11:48:37 -0300

    Pastas da solução

---
+++ certificacao-csharp-pt6-roteiro/certificacao-csharp-pt6-roteiro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace certificacao_csharp_pt6_roteiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
